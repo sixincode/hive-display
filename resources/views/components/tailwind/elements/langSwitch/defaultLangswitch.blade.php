283de5f99ec513271fb05a01ba838abd76932e1a
aalexkarim <contact.alexkarim@gmail.com>
2023-10-11 23:01:16 -0400

    auto-update-1697079673

--- resources/views/components/tailwind/elements/langSwitch/defaultLangswitch.blade.php
+++ resources/views/components/tailwind/elements/langSwitch/defaultLangswitch.blade.php
@@ -24,7 +24,19 @@
       break;
   }
 @endphp
-<x-hive-display-dropdown>
+<div class="text-white/60 flex space-x-2 items-center">
+  <button type="button" class="font-semibold">
+    Français
+  </button>
+  <span class="font-bold">|</span>
+  <button type="button" class="rounded-full">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+    </svg>
+  </button>
+</div>
+
+<!-- <x-hive-display-dropdown>
   <x-slot name="trigger">
     <button
         type="button"
@@ -47,4 +59,4 @@
       @endforeach
     </div>
   </x-slot>
-</x-hive-display-dropdown>
+</x-hive-display-dropdown> -->
